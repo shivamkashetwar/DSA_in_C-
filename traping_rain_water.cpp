fe25386ac977aafc0a93b7b3356d6664d56e7124
shivamkashetwar <168534928+shivamkashetwar@users.noreply.github.com>
2026-09-09 22:22:51 +0530

    Add files via upload

---
+++ traping_rain_water.cpp
@@ -0,0 +1,28 @@
+int main(){
+    vector<int> arr={6,8,0,1,3};
+    stack<int>s;
+    vector<int>ans(arr.size(),0);
+    
+    for(int i=arr.size()-1;i>=0;i--){
+        while(s.size()>0&& s.top()<=arr[i]){
+            s.pop();
+            
+        }
+        
+        if(s.empty()){
+            ans[i]=-1;
+            
+        }else{
+            ans[i]=s.top();
+        }
+        
+        
+        s.push(arr[i]);
+    }
+    
+    for(int val:ans){
+        cout<<val<<" ";
+    }
+    
+    return 0;
+}
