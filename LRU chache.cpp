9c06312f1bd9093a37e810de625a7cd845131f6a
shivamkashetwar <168534928+shivamkashetwar@users.noreply.github.com>
2026-09-11 18:13:54 +0530

    Add files via upload

---
+++ LRU chache.cpp
@@ -0,0 +1,43 @@
+#include<iostream>;
+#include<vector>;
+#include<stack>;
+using namespace std;
+
+vector<int> prevsmalest(vector<int>arr){
+    vector<int>ans(arr.size(),0);
+    stack<int>s;
+    
+    for(int i =0;i<arr.size();i++){
+        while(s.size()>0 && s.top()>=arr[i]){
+            s.pop();
+        }
+        
+        if(s.empty()){
+            ans[i]=-1;
+        }else{
+            ans[i]=s.top();
+        }
+        
+        s.push(arr[i]);
+    }
+    
+    return ans;
+    
+}
+
+int main() {
+    vector<int> arr={3,1,0,8,6};
+    
+    vector<int>ans=prevsmalest(arr);
+    
+    for(int val:ans){
+        cout<<val<< " ";
+        
+    }
+    
+    cout<<endl;
+    
+    
+}
+
+-1 -1 -1 0 0 
