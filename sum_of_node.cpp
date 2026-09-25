2792aced3e6016db576aa37fc9c226e52102be91
shivamkashetwar <168534928+shivamkashetwar@users.noreply.github.com>
2026-09-25 21:34:02 +0530

    Add files via upload

---
+++ sum_of_node.cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int idx = -1;
+
+Node* buildTree(vector<int>& preorder) {
+    idx++;
+
+    if (preorder[idx] == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+}
+
+int sumofnode(Node* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    int leftsum = sumofnode(root->left);
+    int rightsum = sumofnode(root->right);
+
+    return leftsum, rightsum + root->data;
+}
+
+int main() {
+    vector<int> preorder = {
+        1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1
+    };
+
+    Node* root = buildTree(preorder);
+
+    cout << "Height: " << sumofnode(root) << endl;
+
+    return 0;
+}
+
